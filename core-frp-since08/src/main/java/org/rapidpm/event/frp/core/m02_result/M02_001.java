4476d813248bb755ac55964338fe917fd83f5cc0
Sven Ruppert <sven.ruppert@gmail.com>
2018-04-26 14:11:21 +0200

    jdk08 - functional - v001 - added dependency to frp lib

--- core-frp-since08/src/main/java/org/rapidpm/event/frp/core/m02_result/M02_001.java
+++ core-frp-since08/src/main/java/org/rapidpm/event/frp/core/m02_result/M02_001.java
@@ -1,4 +1,30 @@
 package org.rapidpm.event.frp.core.m02_result;
 
 public class M02_001 {
+
+
+  public interface ConverterToInt {
+    Integer toIntegerValue(String input);
+  }
+
+
+  public interface ConverterToString {
+    String toStringValue(Integer input);
+  }
+
+
+  public interface Calculator {
+    Integer calculate(Integer input);
+  }
+
+  public interface Workflow {
+    default String doWork(String input,
+                          ConverterToInt toInt,
+                          ConverterToString toStr,
+                          Calculator calculator) {
+      return toStr.toStringValue(
+          calculator.calculate(
+              toInt.toIntegerValue(input)));
+    }
+  }
 }
